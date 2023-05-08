7650c9218b4e0b7e5cb63ac9ed307301fd983115
Danil <parusovdanik@gmail.com>
2023-05-08 23:11:58 +0300

    second homework is done

--- tests/test_item.py
+++ tests/test_item.py
@@ -13,3 +13,15 @@ def test_first_homework(item):
     assert item.calculate_total_price() == 200000
     item.apply_discount()
     assert item.price == 10000
+
+def test_second_homework(item):
+    assert item.name == "Смартфон"
+    item.name = 'Телефон'
+    assert item.name == 'Телефон'
+
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
